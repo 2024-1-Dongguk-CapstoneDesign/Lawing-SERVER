4940d50a0340715fc12a4fcc70dce279b688bd38
Chaeniiiii <kce2360@gmail.com>
2024-05-11 21:08:22 +0900

    [Setting] #0 - 프로젝트 생성

---
+++ src/main/java/com/capstone/lawing/LawingApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.lawing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LawingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/lawing/LawingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.lawing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
