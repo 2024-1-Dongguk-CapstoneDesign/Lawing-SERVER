c145de75fa6e3aec05b8df2f8ee6769e807f9b55
Chaeniiiii <kce2360@gmail.com>
2024-05-19 16:18:08 +0900

    [Fix] #1 - Entity 수정

---
+++ src/main/java/com/capstone/lawing/domain/BaseTimeEntity.java
@@ -0,0 +1,29 @@
+package com.capstone.lawing.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "createdDate" , nullable = false , updatable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd/HH:mm:ss")
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "modifiedDate" , nullable = false )
+    @DateTimeFormat(pattern = "yyyy-MM-dd/HH:mm:ss")
+    private LocalDateTime modifiedDate;
+
+}
--- src/main/java/com/capstone/lawing/domain/license/License.java
+++ src/main/java/com/capstone/lawing/domain/license/License.java
@@ -1,40 +1,38 @@
 package com.capstone.lawing.domain.license;
 
+import com.capstone.lawing.domain.BaseTimeEntity;
 import com.capstone.lawing.domain.member.Member;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
+
+import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
 @Builder
 @AllArgsConstructor
-public class License {
+public class License extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "licenseId")
+    @Column(name = "license_id")
     private Long id;
 
-    @OneToOne
-    @JoinColumn(name = "memberId")
+    @OneToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
     private Member member;
 
-    @Column(name = "ownerName")
-    @NotNull
+    @Column(name = "owner_name", nullable = false)
     private String ownerName;
 
-    @Column(name = "licenseNumber")
-    @NotNull
+    @Column(name = "license_number", nullable = false)
     private String licenseNumber;
 
-    @Column(name = "licenseBirth")
-    @NotNull
+    @Column(name = "license_birth", nullable = false)
     private String licenseBirth;
 
-    @Column(name = "serialNumber")
-    @NotNull
+    @Column(name = "serial_number", nullable = false)
     private String serialNumber;
 
 }
