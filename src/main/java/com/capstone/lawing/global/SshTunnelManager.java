3d27768964a866b9da402fb65e6fc8238c31e492
Chaeniiiii <kce2360@gmail.com>
2024-06-02 17:26:28 +0900

    [Fix] #12 -test

--- src/main/java/com/capstone/lawing/global/SshTunnelManager.java
+++ src/main/java/com/capstone/lawing/global/SshTunnelManager.java
@@ -1,32 +1,32 @@
-package com.capstone.lawing.global;
-
-
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-
-@Component
-public class SshTunnelManager implements DisposableBean {
-
-    @Value("${ssh.remote}")
-    private String dbEndPoint;
-    private Process sshProcess;
-
-    public SshTunnelManager() throws IOException {
-        startSshTunnel();
-    }
-
-    private void startSshTunnel() throws IOException {
-        sshProcess = new ProcessBuilder("ssh", "-i", "classpath:/key/LawingKey.pem", "-f", "-N", "-L", "3307:"+dbEndPoint+":3306", "ec2-user@15.164.148.174").start();
-    }
-
-    @Override
-    public void destroy() throws Exception {
-        if (sshProcess != null) {
-            sshProcess.destroy();
-        }
-    }
-}
-
+//package com.capstone.lawing.global;
+//
+//
+//import org.springframework.beans.factory.DisposableBean;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+//import java.io.IOException;
+//
+//@Component
+//public class SshTunnelManager implements DisposableBean {
+//
+//    @Value("${ssh.remote}")
+//    private String dbEndPoint;
+//    private Process sshProcess;
+//
+//    public SshTunnelManager() throws IOException {
+//        startSshTunnel();
+//    }
+//
+//    private void startSshTunnel() throws IOException {
+//        sshProcess = new ProcessBuilder("ssh", "-i", "classpath:/key/LawingKey.pem", "-f", "-N", "-L", "3307:"+dbEndPoint+":3306", "ec2-user@15.164.148.174").start();
+//    }
+//
+//    @Override
+//    public void destroy() throws Exception {
+//        if (sshProcess != null) {
+//            sshProcess.destroy();
+//        }
+//    }
+//}
+//
